364ff61c661cda513221fd11f767a8c7882af784
zsjforcn <zhangsanji@gmail.com>
2013-02-27 14:23:52 +0800

    [package] Add "bin" property to package.json for npm to auto generate exection files. Increasing version.

--- acorn.js
+++ acorn.js
@@ -23,7 +23,7 @@
 (function(exports) {
   "use strict";
 
-  exports.version = "0.1.01";
+  exports.version = "0.1.02";
 
   // The main exported interface (under `self.acorn` when in the
   // browser) is a `parse` function that takes a code string and
