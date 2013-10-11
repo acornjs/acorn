a2b2b3712a6bce8ab5fd4c750c869b8b308f62c1
Aparajita Fishman <aparajita@aparajita.com>
2013-10-11 08:55:47 -0400

    Small optimization, strict equality comparisons are way faster than a regex.

--- acorn.js
+++ acorn.js
@@ -1488,9 +1488,10 @@
         var node = startNodeFrom(left);
         node.left = left;
         node.operator = tokVal;
+        var op = tokType;
         next();
         node.right = parseExprOp(parseMaybeUnary(), prec, noIn);
-        var exprNode = finishNode(node, /&&|\|\|/.test(node.operator) ? "LogicalExpression" : "BinaryExpression");
+        var exprNode = finishNode(node, (op === _logicalOR || op === _logicalAND) ? "LogicalExpression" : "BinaryExpression");
         return parseExprOp(exprNode, minPrec, noIn);
       }
     }
