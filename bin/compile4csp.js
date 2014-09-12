4fa3b91d63bae60a751416d9a1f819aab60db2e2
Ingvar Stepanyan <me@rreverser.com>
2014-09-12 12:34:40 +0300

    Use spaces as in all the other code in the project.

--- bin/compile4csp.js
+++ bin/compile4csp.js
@@ -21,40 +21,40 @@ var ast = recast.parse(source);
 var makePredicatePath;
 
 recast.visit(ast, {
-	visitFunctionDeclaration: function (path) {
-		var node = path.node;
-		if (node.id.name === 'makePredicate') {
-			// Found makePredicate function.
-			makePredicatePath = path;
-			// And provide wrapper that collects all the possible results.
-			path.insertAfter(b.expressionStatement(
-				b.assignmentExpression(
-					'=',
-					node.id,
-					b.callExpression(
-						b.identifier('wrapMakePredicate'),
-						[node.id]
-					)
-				)
-			));
-			return false;
-		} else {
-			this.traverse(path);
-		}
-	}
+  visitFunctionDeclaration: function (path) {
+    var node = path.node;
+    if (node.id.name === 'makePredicate') {
+      // Found makePredicate function.
+      makePredicatePath = path;
+      // And provide wrapper that collects all the possible results.
+      path.insertAfter(b.expressionStatement(
+        b.assignmentExpression(
+          '=',
+          node.id,
+          b.callExpression(
+            b.identifier('wrapMakePredicate'),
+            [node.id]
+          )
+        )
+      ));
+      return false;
+    } else {
+      this.traverse(path);
+    }
+  }
 });
 
 // Execute instrumented code and collect possible predicates.
 var makePredicateCache = Object.create(null);
 
 vm.runInNewContext(recast.prettyPrint(ast).code, {
-	wrapMakePredicate: function (makePredicate) {
-		return function (words) {
-			var generatedFn = makePredicate(words);
-			makePredicateCache[words] = generatedFn.toString();
-			return generatedFn;
-		};
-	}
+  wrapMakePredicate: function (makePredicate) {
+    return function (words) {
+      var generatedFn = makePredicate(words);
+      makePredicateCache[words] = generatedFn.toString();
+      return generatedFn;
+    };
+  }
 });
 
 // Remove wrapper.
@@ -62,18 +62,18 @@ makePredicatePath.parentPath.get(makePredicatePath.name + 1).replace();
 
 // And generate it's body as hash of collected inputs/outputs.
 makePredicatePath.get('body', 'body').replace([b.returnStatement(
-	b.memberExpression(
-		b.objectExpression(Object.keys(makePredicateCache).map(function (key) {
-			var funcNode = acorn.parse(makePredicateCache[key]).body[0];
-			return b.property('init', b.literal(key), b.functionExpression(
-				null,
-				funcNode.params,
-				funcNode.body
-			));
-		})),
-		makePredicatePath.get('params', 0).node,
-		true
-	)
+  b.memberExpression(
+    b.objectExpression(Object.keys(makePredicateCache).map(function (key) {
+      var funcNode = acorn.parse(makePredicateCache[key]).body[0];
+      return b.property('init', b.literal(key), b.functionExpression(
+        null,
+        funcNode.params,
+        funcNode.body
+      ));
+    })),
+    makePredicatePath.get('params', 0).node,
+    true
+  )
 )]);
 
 // Finally, save transformed AST to file with preserved formatting.
