4fde5da90853ba1c6a8aad587051f5052e278e82
Marijn Haverbeke <marijnh@gmail.com>
2015-04-07 10:21:39 +0200

    Revise module loader locating of acorn module again
    To work in Web workers
    
    Closes #239

--- bin/build-acorn.js
+++ bin/build-acorn.js
@@ -21,7 +21,7 @@ function acornShim(file) {
     tr._transform = function(chunk, _, callback) {
       if (!sent) {
         sent = true
-        callback(null, "module.exports = (typeof window != 'undefined' && window.acorn) || _dereq_(\"./acorn\")")
+        callback(null, "module.exports = typeof acorn != 'undefined' ? acorn : _dereq_(\"./acorn\")")
       } else {
         callback()
       }
