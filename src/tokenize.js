c5ced2c6acb62050ca1f4a4d1e20dc586d43f780
Marijn Haverbeke <marijnh@gmail.com>
2015-05-27 14:47:02 +0200

    Another refinement to Rhino-detection
    Issue #256

--- src/tokenize.js
+++ src/tokenize.js
@@ -26,7 +26,7 @@ export class Token {
 const pp = Parser.prototype
 
 // Are we running under Rhino?
-const isRhino = typeof Packages == "object" && String(Packages) == "[JavaPackage ]"
+const isRhino = typeof Packages == "object" && Object.prototype.toString.call(Packages) == "[object JavaPackage]"
 
 // Move to the next token
 
