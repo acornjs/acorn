934f9dea37aa296925c4496f01060d45abf88da7
Jesse McCarthy <git_commits@jessemccarthy.net>
2015-06-29 09:43:31 -0400

    Add tests for `use strict` impostor.
    (Failing.)

--- test/tests.js
+++ test/tests.js
@@ -21205,6 +21205,14 @@ test("with (x) foo = bar;", {
   }
 });
 
+// Test that innocuous string that evaluates to `use strict` is not promoted to
+// Use Strict directive.
+test("'use\\x20strict'; with (x) foo = bar;", {});
+
+// Test that innocuous string that evaluates to `use strict` is not promoted to
+// Use Strict directive.
+test('"use\\x20strict"; with (x) foo = bar;', {});
+
 test("with (x) { foo = bar }", {
   type: "Program",
   body: [
