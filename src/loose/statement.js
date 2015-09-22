acornjs/acorn

commit cc88ecc4227fceb09898ba802b9f2cdb175b4468

[loose parser] Don't allow non-string expressions as import source
Issue marijnh/tern#643

src/loose/statement.js
@@ -359,7 +359,7 @@ lp.parseImport = function() {
       this.eat(tt.comma)
     }
     node.specifiers = this.parseImportSpecifierList()
-    node.source = this.eatContextual("from") ? this.parseExprAtom() : this.dummyString()
+    node.source = this.eatContextual("from") && this.type == tt.string ? this.parseExprAtom() : this.dummyString()
     if (elt) node.specifiers.unshift(elt)
   }
   this.semicolon()

commit 331301e7f5bfdafb7ad92de4af07b3544dc09088

[loose parser] Fix bug introduced in cc88ecc4227fceb
Issue marijnh/tern#643

src/loose/statement.js
@@ -359,7 +359,7 @@ lp.parseImport = function() {
       this.eat(tt.comma)
     }
     node.specifiers = this.parseImportSpecifierList()
-    node.source = this.eatContextual("from") && this.type == tt.string ? this.parseExprAtom() : this.dummyString()
+    node.source = this.eatContextual("from") && this.tok.type == tt.string ? this.parseExprAtom() : this.dummyString()
     if (elt) node.specifiers.unshift(elt)
   }
   this.semicolon()