0d38744dbc749ede7a52a13e29fabfaaeec9462b
Marijn Haverbeke <marijn@haverbeke.nl>
2016-06-30 11:34:47 +0200

    Slightly adjust patch a062434d4
    Issue #424

--- src/expression.js
+++ src/expression.js
@@ -607,27 +607,27 @@ pp.parseFunctionBody = function(node, isArrowFunction) {
   // If this is a strict mode function, verify that argument names
   // are not repeated, and it does not try to bind the words `eval`
   // or `arguments`.
-  let refUseStrict = (!isExpression && node.body.body.length && this.isUseStrict(node.body.body[0])) ? node.body.body[0] : null;
-  if (this.strict || refUseStrict) {
+  let useStrict = (!isExpression && node.body.body.length && this.isUseStrict(node.body.body[0])) ? node.body.body[0] : null;
+  if (this.strict || useStrict) {
     let oldStrict = this.strict
     this.strict = true
     if (node.id)
       this.checkLVal(node.id, true)
-    this.checkParams(node, refUseStrict)
+    this.checkParams(node, useStrict)
     this.strict = oldStrict
   } else if (isArrowFunction) {
-    this.checkParams(node, refUseStrict)
+    this.checkParams(node, useStrict)
   }
 }
 
 // Checks function params for various disallowed patterns such as using "eval"
 // or "arguments" and duplicate parameters.
 
-pp.checkParams = function(node, refUseStrict) {
+pp.checkParams = function(node, useStrict) {
     let nameHash = {}
     for (let i = 0; i < node.params.length; i++) {
-      if (node.params.type !== "Identifier" && refUseStrict && this.options.ecmaVersion >= 7)
-        this.raiseRecoverable(refUseStrict.start, "Illegal 'use strict' directive in function with non-simple parameter list");
+      if (useStrict && this.options.ecmaVersion >= 7 && node.params.type !== "Identifier")
+        this.raiseRecoverable(useStrict.start, "Illegal 'use strict' directive in function with non-simple parameter list");
       this.checkLVal(node.params[i], true, nameHash)
     }
 }
