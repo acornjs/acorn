f36d882b2504f3001722d908133afe134049884c
Marijn Haverbeke <marijn@haverbeke.nl>
2017-03-28 11:07:13 +0200

    Make linter happy

--- src/util.js
+++ src/util.js
@@ -1,13 +1,11 @@
-const { hasOwnProperty, toString } = Object.prototype
+const {hasOwnProperty, toString} = Object.prototype
 
 // Checks if an object has a property.
 
-const has = (obj, propName) => (
-  hasOwnProperty.call(obj, propName)
-)
+export function has(obj, propName) {
+  return hasOwnProperty.call(obj, propName)
+}
 
-const isArray = Array.isArray || ((obj) => (
+export const isArray = Array.isArray || ((obj) => (
   toString.call(obj) === "[object Array]"
 ))
-
-export { has, isArray }
