acornjs/acorn

commit ccfd59d044fa004f852cc4105d09782e0192e078

fix parsing errors about template literals (fixes #575)

src/expression.js
@@ -814,6 +814,13 @@ pp.parseIdent = function(liberal, isBinding) {
     node.name = this.value
   } else if (this.type.keyword) {
     node.name = this.type.keyword
+
+    // To fix https://github.com/ternjs/acorn/issues/575
+    // `class` and `function` keywords push new context into this.context.
+    // But there is no chance to pop the context if the keyword is consumed as an identifier such as a property name.
+    if (node.name === "class" || node.name === "function") {
+      this.context.pop()
+    }
   } else {
     this.unexpected()
   }

test/tests-template-literal-revision.js
@@ -549,3 +549,8 @@ test("foo`\\unicode\\\\`", {
   ],
   sourceType: "script"
 }, {ecmaVersion: 9})
+
+test("`${ {class: 1} }`", {}, { ecmaVersion: 9 })
+test("`${ {delete: 1} }`", {}, { ecmaVersion: 9 })
+test("`${ {enum: 1} }`", {}, { ecmaVersion: 9 })
+test("`${ {function: 1} }`", {}, { ecmaVersion: 9 })

commit 26054083289df8f0fe97afcaeef17b5f9f103c3c

Fix token context corruption introduced by ccfd59d044
Issue #607

src/expression.js
@@ -835,7 +835,9 @@ pp.parseIdent = function(liberal, isBinding) {
     // To fix https://github.com/ternjs/acorn/issues/575
     // `class` and `function` keywords push new context into this.context.
     // But there is no chance to pop the context if the keyword is consumed as an identifier such as a property name.
-    if (node.name === "class" || node.name === "function") {
+    // If the previous token is a dot, this does not apply because the context-managing code already ignored the keyword
+    if ((node.name === "class" || node.name === "function") &&
+        (this.lastTokEnd !== this.lastTokStart + 1 || this.input.charCodeAt(this.lastTokStart) !== 46)) {
       this.context.pop()
     }
   } else {

test/tests.js
@@ -29217,3 +29217,4 @@ test("try {} catch (foo) { function x(foo) {} }", {}, {ecmaVersion: 6})
 test("'use strict'; let foo = function foo() {}", {}, {ecmaVersion: 6})
 
 test("/**/ --> comment\n", {})
+test("x.class++", {})