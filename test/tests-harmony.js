acornjs/acorn

commit 58603fd85ae4655e62df5dffaeb4853c3a198e20

Only use the no-asi rule after let when the next char is {
Issue #765

acorn/src/statement.js
@@ -37,8 +37,8 @@ pp.isLet = function() {
   skipWhiteSpace.lastIndex = this.pos
   let skip = skipWhiteSpace.exec(this.input)
   let next = this.pos + skip[0].length, nextCh = this.input.charCodeAt(next)
-  if (lineBreak.test(this.input.slice(this.end, next))) return false
-  if (nextCh === 91 || nextCh === 123) return true // '{' and '['
+  if (nextCh === 123 && !lineBreak.test(this.input.slice(this.end, next)) // '{'
+      || nextCh === 91) return true // '['
   if (isIdentifierStart(nextCh, true)) {
     let pos = next + 1
     while (isIdentifierChar(this.input.charCodeAt(pos), true)) ++pos

bin/test262.whitelist
@@ -153,7 +153,6 @@ language/statements/class/syntax/early-errors/class-definition-evaluation-block-
 language/statements/class/syntax/early-errors/class-definition-evaluation-scriptbody-duplicate-binding.js (default)
 language/statements/class/syntax/early-errors/class-definition-evaluation-scriptbody-duplicate-binding.js (strict mode)
 language/statements/const/syntax/const-declaring-let-split-across-two-lines.js (default)
-language/statements/for-await-of/let-block-with-newline.js (default)
 language/statements/for-await-of/let-identifier-with-newline.js (default)
 language/statements/for/head-let-bound-names-in-stmt.js (default)
 language/statements/for/head-let-bound-names-in-stmt.js (strict mode)
@@ -163,9 +162,7 @@ language/statements/for-in/head-const-bound-names-let.js (default)
 language/statements/for-in/head-let-bound-names-in-stmt.js (default)
 language/statements/for-in/head-let-bound-names-in-stmt.js (strict mode)
 language/statements/for-in/head-let-bound-names-let.js (default)
-language/statements/for-in/let-block-with-newline.js (default)
 language/statements/for-in/let-identifier-with-newline.js (default)
-language/statements/for/let-block-with-newline.js (default)
 language/statements/for/let-identifier-with-newline.js (default)
 language/statements/for-of/head-const-bound-names-in-stmt.js (default)
 language/statements/for-of/head-const-bound-names-in-stmt.js (strict mode)
@@ -173,7 +170,6 @@ language/statements/for-of/head-const-bound-names-let.js (default)
 language/statements/for-of/head-let-bound-names-in-stmt.js (default)
 language/statements/for-of/head-let-bound-names-in-stmt.js (strict mode)
 language/statements/for-of/head-let-bound-names-let.js (default)
-language/statements/for-of/let-block-with-newline.js (default)
 language/statements/for-of/let-identifier-with-newline.js (default)
 language/statements/if/if-gen-else-gen.js (default)
 language/statements/if/if-gen-else-stmt.js (default)
@@ -182,9 +178,7 @@ language/statements/if/if-stmt-else-gen.js (default)
 language/statements/if/labelled-fn-stmt-first.js (default)
 language/statements/if/labelled-fn-stmt-lone.js (default)
 language/statements/if/labelled-fn-stmt-second.js (default)
-language/statements/if/let-block-with-newline.js (default)
 language/statements/if/let-identifier-with-newline.js (default)
-language/statements/labeled/let-block-with-newline.js (default)
 language/statements/labeled/let-identifier-with-newline.js (default)
 language/statements/let/syntax/identifier-let-disallowed-as-boundname.js (default)
 language/statements/let/syntax/let-let-declaration-split-across-two-lines.js (default)
@@ -268,9 +262,7 @@ language/statements/switch/syntax/redeclaration/var-declaration-attempt-to-redec
 language/statements/switch/syntax/redeclaration/var-declaration-attempt-to-redeclare-with-generator-declaration.js (strict mode)
 language/statements/try/early-catch-var.js (default)
 language/statements/try/early-catch-var.js (strict mode)
-language/statements/while/let-block-with-newline.js (default)
 language/statements/while/let-identifier-with-newline.js (default)
-language/statements/with/let-block-with-newline.js (default)
 language/statements/with/let-identifier-with-newline.js (default)
 language/white-space/mongolian-vowel-separator.js (default)
 language/white-space/mongolian-vowel-separator.js (strict mode)

commit 8dbe8f859ec817198c38fe9c52d96e7ce56e4e01

Fix isLet for sole body of statements
As a drive-by fix, forbid constructions like

    if (false)
      label: function a() {}

and

    for (let
    {x} of list) process(x);

the latter being a regression from 58603fd85a.

Fixes #772
Fixes #774

acorn/src/statement.js
@@ -32,13 +32,19 @@ pp.parseTopLevel = function(node) {
 
 const loopLabel = {kind: "loop"}, switchLabel = {kind: "switch"}
 
-pp.isLet = function() {
+pp.isLet = function(context) {
   if (this.options.ecmaVersion < 6 || !this.isContextual("let")) return false
   skipWhiteSpace.lastIndex = this.pos
   let skip = skipWhiteSpace.exec(this.input)
   let next = this.pos + skip[0].length, nextCh = this.input.charCodeAt(next)
-  if (nextCh === 123 && !lineBreak.test(this.input.slice(this.end, next)) // '{'
-      || nextCh === 91) return true // '['
+  // For ambiguous cases, determine if a LexicalDeclaration (or only a
+  // Statement) is allowed here. If context is not empty then only a Statement
+  // is allowed. However, `let [` is an explicit negative lookahead for
+  // ExpressionStatement, so special-case it first.
+  if (nextCh === 91) return true // '['
+  if (context) return false
+
+  if (nextCh === 123) return true // '{'
   if (isIdentifierStart(nextCh, true)) {
     let pos = next + 1
     while (isIdentifierChar(this.input.charCodeAt(pos), true)) ++pos
@@ -73,7 +79,7 @@ pp.isAsyncFunction = function() {
 pp.parseStatement = function(context, topLevel, exports) {
   let starttype = this.type, node = this.startNode(), kind
 
-  if (this.isLet()) {
+  if (this.isLet(context)) {
     starttype = tt._var
     kind = "let"
   }
@@ -88,7 +94,10 @@ pp.parseStatement = function(context, topLevel, exports) {
   case tt._do: return this.parseDoStatement(node)
   case tt._for: return this.parseForStatement(node)
   case tt._function:
-    if ((context && (this.strict || context !== "if")) && this.options.ecmaVersion >= 6) this.unexpected()
+    // Function as sole body of either an if statement or a labeled statement
+    // works, but not when it is part of a labeled statement that is the sole
+    // body of an if statement.
+    if ((context && (this.strict || context !== "if" && context !== "label")) && this.options.ecmaVersion >= 6) this.unexpected()
     return this.parseFunctionStatement(node, false, !context)
   case tt._class:
     if (context) this.unexpected()
@@ -385,11 +394,7 @@ pp.parseLabeledStatement = function(node, maybeName, expr, context) {
     } else break
   }
   this.labels.push({name: maybeName, kind, statementStart: this.start})
-  node.body = this.parseStatement(context)
-  if (node.body.type === "ClassDeclaration" ||
-      node.body.type === "VariableDeclaration" && node.body.kind !== "var" ||
-      node.body.type === "FunctionDeclaration" && (this.strict || node.body.generator || node.body.async))
-    this.raiseRecoverable(node.body.start, "Invalid labeled declaration")
+  node.body = this.parseStatement(context ? context.indexOf("label") === -1 ? context + "label" : context : "label")
   this.labels.pop()
   node.label = expr
   return this.finishNode(node, "LabeledStatement")

bin/test262.whitelist
@@ -92,15 +92,6 @@ language/module-code/early-export-global.js (default)
 language/module-code/early-export-global.js (strict mode)
 language/module-code/early-export-unresolvable.js (default)
 language/module-code/early-export-unresolvable.js (strict mode)
-language/statements/for-await-of/let-identifier-with-newline.js (default)
-language/statements/for-in/let-identifier-with-newline.js (default)
-language/statements/for/let-identifier-with-newline.js (default)
-language/statements/for-of/let-identifier-with-newline.js (default)
-language/statements/if/labelled-fn-stmt-first.js (default)
-language/statements/if/labelled-fn-stmt-lone.js (default)
-language/statements/if/labelled-fn-stmt-second.js (default)
-language/statements/if/let-identifier-with-newline.js (default)
-language/statements/labeled/let-identifier-with-newline.js (default)
 language/statements/switch/syntax/redeclaration/async-function-declaration-attempt-to-redeclare-with-async-function-declaration.js (default)
 language/statements/switch/syntax/redeclaration/async-function-declaration-attempt-to-redeclare-with-async-function-declaration.js (strict mode)
 language/statements/switch/syntax/redeclaration/async-function-declaration-attempt-to-redeclare-with-async-generator-declaration.js (default)
@@ -150,7 +141,5 @@ language/statements/switch/syntax/redeclaration/var-declaration-attempt-to-redec
 language/statements/switch/syntax/redeclaration/var-declaration-attempt-to-redeclare-with-generator-declaration.js (strict mode)
 language/statements/try/early-catch-var.js (default)
 language/statements/try/early-catch-var.js (strict mode)
-language/statements/while/let-identifier-with-newline.js (default)
-language/statements/with/let-identifier-with-newline.js (default)
 language/white-space/mongolian-vowel-separator.js (default)
 language/white-space/mongolian-vowel-separator.js (strict mode)

test/tests-asyncawait.js
@@ -3507,4 +3507,4 @@ test(
 
 test("({ async delete() {} })", {}, {ecmaVersion: 8})
 
-testFail("abc: async function a() {}", "Invalid labeled declaration (1:5)", {ecmaVersion: 8})
+testFail("abc: async function a() {}", "Unexpected token (1:5)", {ecmaVersion: 8})

test/tests-harmony.js
@@ -6031,6 +6031,109 @@ test("for (let x of list) process(x);", {
   locations: true
 });
 
+test("for (let\n{x} of list) process(x);", {
+  type: "Program",
+  body: [{
+    type: "ForOfStatement",
+    left: {
+      type: "VariableDeclaration",
+      declarations: [{
+        type: "VariableDeclarator",
+        id: {
+          type: "ObjectPattern",
+          loc: {
+            start: {line: 2, column: 0},
+            end: {line: 2, column: 3}
+          },
+          properties: [{
+            type: "Property",
+            kind: "init",
+            key: {
+              type: "Identifier",
+              name: "x",
+              loc: {
+                start: {line: 2, column: 1},
+                end: {line: 2, column: 2}
+              }
+            },
+            value: {
+              type: "Identifier",
+              name: "x",
+              loc: {
+                start: {line: 2, column: 1},
+                end: {line: 2, column: 2}
+              }
+            },
+            loc: {
+              start: {line: 2, column: 1},
+              end: {line: 2, column: 2}
+            }
+          }]
+        },
+        init: null,
+        loc: {
+          start: {line: 2, column: 0},
+          end: {line: 2, column: 3}
+        }
+      }],
+      kind: "let",
+      loc: {
+        start: {line: 1, column: 5},
+        end: {line: 2, column: 3}
+      }
+    },
+    right: {
+      type: "Identifier",
+      name: "list",
+      loc: {
+        start: {line: 2, column: 7},
+        end: {line: 2, column: 11}
+      }
+    },
+    body: {
+      type: "ExpressionStatement",
+      expression: {
+        type: "CallExpression",
+        callee: {
+          type: "Identifier",
+          name: "process",
+          loc: {
+            start: {line: 2, column: 13},
+            end: {line: 2, column: 20}
+          }
+        },
+        arguments: [{
+          type: "Identifier",
+          name: "x",
+          loc: {
+            start: {line: 2, column: 21},
+            end: {line: 2, column: 22}
+          }
+        }],
+        loc: {
+          start: {line: 2, column: 13},
+          end: {line: 2, column: 23}
+        }
+      },
+      loc: {
+        start: {line: 2, column: 13},
+        end: {line: 2, column: 24}
+      }
+    },
+    loc: {
+      start: {line: 1, column: 0},
+      end: {line: 2, column: 24}
+    }
+  }],
+  loc: {
+    start: {line: 1, column: 0},
+    end: {line: 2, column: 24}
+  }
+}, {
+  ecmaVersion: 6,
+  locations: true
+});
+
 // Harmony: Class (strawman)
 
 test("var A = class extends B {}", {
@@ -14535,7 +14638,7 @@ testFail("var [x]", "Complex binding patterns require an initialization value (1
 testFail("var _𖫵 = 11;", "Unexpected character '𖫵' (1:5)", {ecmaVersion: 6});
 testFail("var 𫠞_ = 12;", "Unexpected character '𫠞' (1:4)", {ecmaVersion: 6});
 testFail("var 𫠝_ = 10;", "Unexpected character '𫠝' (1:4)", {ecmaVersion: 5});
-testFail("if (1) let x = 10;", "Unexpected token (1:7)", {ecmaVersion: 6});
+testFail("if (1) let x = 10;", "Unexpected token (1:11)", {ecmaVersion: 6});
 testFail("for (;;) const x = 10;", "Unexpected token (1:9)", {ecmaVersion: 6});
 testFail("while (1) function foo(){}", "Unexpected token (1:10)", {ecmaVersion: 6});
 testFail("if (1) ; else class Cls {}", "Unexpected token (1:14)", {ecmaVersion: 6});
@@ -15949,9 +16052,12 @@ testFail("export { default} from './y.js';\nexport default 42;",
 testFail("export * from foo", "Unexpected token (1:14)", {sourceType: "module", ecmaVersion: 6, loose: false});
 testFail("export { bar } from foo", "Unexpected token (1:20)", {sourceType: "module", ecmaVersion: 6, loose: false});
 
-testFail("foo: class X {}", "Invalid labeled declaration (1:5)", {ecmaVersion: 6})
+testFail("foo: class X {}", "Unexpected token (1:5)", {ecmaVersion: 6})
 
-testFail("'use strict'; bar: function x() {}", "Invalid labeled declaration (1:19)", {ecmaVersion: 6})
+testFail("'use strict'; bar: function x() {}", "Unexpected token (1:19)", {ecmaVersion: 6})
+
+testFail("'use strict'; bar: function* x() {}", "Unexpected token (1:19)", {ecmaVersion: 6})
+testFail("bar: function* x() {}", "Unexpected token (1:13)", {ecmaVersion: 6})
 
 testFail("({x, y}) = {}", "Parenthesized pattern (1:0)", {ecmaVersion: 6})
 