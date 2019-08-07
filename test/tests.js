42982add8383fa9bb78ed1b66c4e42a4ff2deaad
龙腾道 <LongTengDao@LongTengDao.com>
2019-08-07 17:17:04 +0800

    Update tests.js

--- test/tests.js
+++ test/tests.js
@@ -7,6 +7,34 @@ if (typeof exports != "undefined") {
   var acorn = require("../acorn");
 }
 
+test("new Object", {
+  type: "Program",
+  start: 0,
+  end: 10,
+  body: [
+    {
+      type: "ExpressionStatement",
+      start: 0,
+      end: 10,
+      expression: {
+        type: "NewExpression",
+        start: 0,
+        end: 10,
+        callee: {
+          type: "Identifier",
+          start: 4,
+          end: 10,
+          name: "Object"
+        },
+        arguments: []
+      }
+    }
+  ],
+  sourceType: "script"
+}, {
+  allowReserved: "never"
+});
+
 test("this\n", {
   type: "Program",
   body: [
