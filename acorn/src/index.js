88c2669600a6d33134755fb91ff8ab9cd9ebc284
Marijn Haverbeke <marijn@haverbeke.nl>
2020-10-05 09:52:26 +0200

    Mark version 7.4.1

--- acorn/src/index.js
+++ acorn/src/index.js
@@ -31,7 +31,7 @@ import {isIdentifierChar, isIdentifierStart} from "./identifier"
 import {Token} from "./tokenize"
 import {isNewLine, lineBreak, lineBreakG, nonASCIIwhitespace} from "./whitespace"
 
-export const version = "7.4.0"
+export const version = "7.4.1"
 export {
   Parser,
   defaultOptions,
