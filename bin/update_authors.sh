aaf53acc9d859e2d628264cfbf8e2a703a81b545
Timo Tijhof <krinklemail@gmail.com>
2021-02-01 22:25:37 +0000

    Update authors list, use .mailmap file
    Use a git standard .mailmap file for removing duplicates and choosing
    which names to use for authors that used different display names on
    different commits.
    
    This means `git log` and `git show` naturally pick it up, as well
    as summary commands like `git shortlog -s` and `git shortlog -sn`,
    and various GUIs for Git.
    
    I've also added -f to the sort command as it appeared to (mostly)
    be sorted case-insensitively already. Without this option, running
    the script resulted in all lowercase names being moved to the end
    of the list (tried both macOS/BSD's default sort, and the one from
    gnu-coreutils). I'm guessing the previous run was done on a machine
    with a different implementation or override present that uses some
    kind of hybrid by default.

--- bin/update_authors.sh
+++ bin/update_authors.sh
@@ -1,3 +1,3 @@
 echo "List of Acorn contributors. Updated before every release." > AUTHORS
 echo >> AUTHORS
-git log --format='%aN' | grep -v 'Adrian Heine né Lang' | grep -v abraidwood | grep -v Rich-Harris | grep -v ForbesLindesay | sort -u >> AUTHORS
+git log --format='%aN' | sort -uf >> AUTHORS
