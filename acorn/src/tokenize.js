556a7ab62dd999c9024ed0e0920ebb5f6521c4de
Marijn Haverbeke <marijn@haverbeke.nl>
2021-05-01 16:23:45 +0200

    Make fullCharCodeAtPos more accurate
    FIX: Fix a bug in UTF-16 decoding that would read characters incorrectly in
    some circumstances.
    
    Closes https://github.com/acornjs/acorn/issues/1032

--- acorn/src/tokenize.js
+++ acorn/src/tokenize.js
@@ -93,9 +93,9 @@ pp.readToken = function(code) {
 
 pp.fullCharCodeAtPos = function() {
   let code = this.input.charCodeAt(this.pos)
-  if (code <= 0xd7ff || code >= 0xe000) return code
+  if (code <= 0xd7ff || code >= 0xdc00) return code
   let next = this.input.charCodeAt(this.pos + 1)
-  return (code << 10) + next - 0x35fdc00
+  return next <= 0xdbff || next >= 0xe000 ? code : (code << 10) + next - 0x35fdc00
 }
 
 pp.skipBlockComment = function() {
