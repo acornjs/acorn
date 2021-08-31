c1df6127040582e4bfdf050ddb6bd83c2ffdb991
成仕伟 <chengsw@heywhale.com>
2021-08-31 17:18:39 +0800

    Cli support parsing multiple files
    Close #825

--- acorn/src/bin/acorn.js
+++ acorn/src/bin/acorn.js
@@ -2,21 +2,24 @@ import {basename} from "path"
 import {readFileSync as readFile} from "fs"
 import * as acorn from "acorn"
 
-let infile, forceFile, silent = false, compact = false, tokenize = false
+let inputFilePaths = [], forceFileName = false, fileMode = false, silent = false, compact = false, tokenize = false
 const options = {}
 
 function help(status) {
   const print = (status === 0) ? console.log : console.error
   print("usage: " + basename(process.argv[1]) + " [--ecma3|--ecma5|--ecma6|--ecma7|--ecma8|--ecma9|...|--ecma2015|--ecma2016|--ecma2017|--ecma2018|...]")
-  print("        [--tokenize] [--locations] [---allow-hash-bang] [--allow-await-outside-function] [--compact] [--silent] [--module] [--help] [--] [infile]")
+  print("        [--tokenize] [--locations] [---allow-hash-bang] [--allow-await-outside-function] [--compact] [--silent] [--module] [--help] [--] [<infile>...]")
   process.exit(status)
 }
 
 for (let i = 2; i < process.argv.length; ++i) {
   const arg = process.argv[i]
-  if ((arg === "-" || arg[0] !== "-") && !infile) infile = arg
-  else if (arg === "--" && !infile && i + 2 === process.argv.length) forceFile = infile = process.argv[++i]
-  else if (arg === "--locations") options.locations = true
+  if (arg[0] !== "-" || arg === "-") inputFilePaths.push(arg)
+  else if (arg === "--") {
+    inputFilePaths.push(...process.argv.slice(i + 1))
+    forceFileName = true
+    break
+  } else if (arg === "--locations") options.locations = true
   else if (arg === "--allow-hash-bang") options.allowHashBang = true
   else if (arg === "--allow-await-outside-function") options.allowAwaitOutsideFunction = true
   else if (arg === "--silent") silent = true
@@ -33,31 +36,34 @@ for (let i = 2; i < process.argv.length; ++i) {
   }
 }
 
-function run(code) {
-  let result
+function run(codeList) {
+  let result = [], fileIdx = 0
   try {
-    if (!tokenize) {
-      result = acorn.parse(code, options)
-    } else {
-      result = []
-      let tokenizer = acorn.tokenizer(code, options), token
-      do {
-        token = tokenizer.getToken()
-        result.push(token)
-      } while (token.type !== acorn.tokTypes.eof)
-    }
+    codeList.forEach((code, idx) => {
+      fileIdx = idx
+      if (!tokenize) {
+        result = acorn.parse(code, options)
+        options.program = result
+      } else {
+        let tokenizer = acorn.tokenizer(code, options), token
+        do {
+          token = tokenizer.getToken()
+          result.push(token)
+        } while (token.type !== acorn.tokTypes.eof)
+      }
+    })
   } catch (e) {
-    console.error(infile && infile !== "-" ? e.message.replace(/\(\d+:\d+\)$/, m => m.slice(0, 1) + infile + " " + m.slice(1)) : e.message)
+    console.error(fileMode ? e.message.replace(/\(\d+:\d+\)$/, m => m.slice(0, 1) + inputFilePaths[fileIdx] + " " + m.slice(1)) : e.message)
     process.exit(1)
   }
   if (!silent) console.log(JSON.stringify(result, null, compact ? null : 2))
 }
 
-if (forceFile || infile && infile !== "-") {
-  run(readFile(infile, "utf8"))
+if (fileMode = inputFilePaths.length && (forceFileName || !inputFilePaths.includes("-") || inputFilePaths.length !== 1)) {
+  run(inputFilePaths.map(path => readFile(path, "utf8")))
 } else {
   let code = ""
   process.stdin.resume()
   process.stdin.on("data", chunk => code += chunk)
-  process.stdin.on("end", () => run(code))
+  process.stdin.on("end", () => run([code]))
 }
