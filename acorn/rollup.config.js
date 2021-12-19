e566b2f3cd19ba9b5e0a50c53ed3dddb84dde3a4
Daniel Nalborczyk <dnalborczyk@gmail.com>
2021-12-19 14:33:36 -0500

    Consolidate rollup configs

--- acorn/rollup.config.js
+++ acorn/rollup.config.js
@@ -1,19 +1,33 @@
 import buble from "@rollup/plugin-buble"
 
-export default {
-  input: "acorn/src/index.js",
-  output: [
-    {
-      file: "acorn/dist/acorn.js",
-      format: "umd",
-      name: "acorn"
+export default [
+  {
+    input: "acorn/src/index.js",
+    output: [
+      {
+        file: "acorn/dist/acorn.js",
+        format: "umd",
+        name: "acorn"
+      },
+      {
+        file: "acorn/dist/acorn.mjs",
+        format: "es"
+      }
+    ],
+    plugins: [
+      buble({transforms: {dangerousForOf: true}})
+    ]
+  },
+  {
+    external: ["acorn", "fs", "path"],
+    input: "acorn/src/bin/acorn.js",
+    output: {
+      file: "acorn/dist/bin.js",
+      format: "cjs",
+      paths: {acorn: "./acorn.js"}
     },
-    {
-      file: "acorn/dist/acorn.mjs",
-      format: "es"
-    }
-  ],
-  plugins: [
-    buble({transforms: {dangerousForOf: true}})
-  ]
-}
+    plugins: [
+      buble({transforms: {dangerousForOf: true}})
+    ]
+  }
+]
