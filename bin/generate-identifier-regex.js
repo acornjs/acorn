0c222f129454060b6a7b13154defbfa4e3e8910a
dnalborczyk <dnalborczyk@gmail.com>
2022-01-11 03:59:03 -0500

    Generate identifier unicode data to source files

--- bin/generate-identifier-regex.js
+++ bin/generate-identifier-regex.js
@@ -1,13 +1,15 @@
 "use strict"
 
 // Which Unicode version should be used?
-let pkg = require("../package.json")
-let dependencies = Object.keys(pkg.devDependencies)
-let unicodeVersion = dependencies.find((name) => /^@unicode\/unicode-\d/.test(name))
+const fs = require("fs")
+const path = require("path")
+const pkg = require("../package.json")
+const dependencies = Object.keys(pkg.devDependencies)
+const unicodeVersion = dependencies.find((name) => /^@unicode\/unicode-\d/.test(name))
 
-let start = require(unicodeVersion + "/Binary_Property/ID_Start/code-points.js").filter(ch => ch > 0x7f)
+const start = require(unicodeVersion + "/Binary_Property/ID_Start/code-points.js").filter(ch => ch > 0x7f)
 let last = -1
-let cont = [0x200c, 0x200d].concat(require(unicodeVersion + "/Binary_Property/ID_Continue/code-points.js")
+const cont = [0x200c, 0x200d].concat(require(unicodeVersion + "/Binary_Property/ID_Continue/code-points.js")
   .filter(ch => ch > 0x7f && search(start, ch, last + 1) === -1))
 
 function search(arr, ch, starting) {
@@ -17,12 +19,13 @@ function search(arr, ch, starting) {
 }
 
 function esc(code) {
-  let hex = code.toString(16)
+  const hex = code.toString(16)
   return hex.length <= 2 ? "\\x" + hex.padStart(2, "0") : "\\u" + hex.padStart(4, "0")
 }
 
 function generate(chars) {
-  let astral = [], re = ""
+  const astral = []
+  let re = ""
   for (let i = 0, at = 0x10000; i < chars.length; i++) {
     let from = chars[i], to = from
     while (i < chars.length - 1 && chars[i + 1] === to + 1) { i++; to++ }
@@ -38,23 +41,22 @@ function generate(chars) {
   return {nonASCII: re, astral: astral}
 }
 
-let startData = generate(start), contData = generate(cont)
+const startData = generate(start), contData = generate(cont)
 
-let code = [
-  `    let nonASCIIidentifierStartChars = "${startData.nonASCII}"`,
-  `    let nonASCIIidentifierChars = "${contData.nonASCII}"`,
-  `    const astralIdentifierStartCodes = ${JSON.stringify(startData.astral)}`,
-  `    const astralIdentifierCodes = ${JSON.stringify(contData.astral)}`
-]
+const astralIdentifierStartCodes = "export default " + JSON.stringify(startData.astral).split(",").join(", ")
+const astralIdentifierCodes = "export default " + JSON.stringify(contData.astral).split(",").join(", ")
+const nonASCIIidentifierStartChars = "export default \"" + startData.nonASCII + "\""
+const nonASCIIidentifierChars = "export default \"" + contData.nonASCII + "\""
 
-if (process.argv.length !== 3) {
-  console.log(code.join("\n"))
-} else {
-  let {readFile} = require("fs")
-  readFile(process.argv[2], "utf8", function(err, data) {
-    if (err) throw err
-    for (let line of code)
-      data = data.replace(new RegExp(/.* = /.exec(line)[0] + ".*"), line)
-    process.stdout.write(data)
-  })
+const comment = "// This file was generated. Do not modify manually!"
+
+function writeGeneratedFile(filename, content) {
+  fs.writeFileSync(path.resolve("./acorn/src/generated", filename + ".js"), comment + "\n" + content + "\n", "utf8")
 }
+
+writeGeneratedFile("astralIdentifierStartCodes", astralIdentifierStartCodes)
+writeGeneratedFile("astralIdentifierCodes", astralIdentifierCodes)
+writeGeneratedFile("nonASCIIidentifierStartChars", nonASCIIidentifierStartChars)
+writeGeneratedFile("nonASCIIidentifierChars", nonASCIIidentifierChars)
+
+console.log("Done. The generated files must be committed.")
