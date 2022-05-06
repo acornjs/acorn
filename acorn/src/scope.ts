97b3ea055e3f2782da0cde1794f7bb0fad650819
Nino Filiu <nino.filiu@gmail.com>
2022-05-06 23:09:44 +0200

    add missing types in Scope by rewriting the constructor in a more TS-idiomatic way

--- acorn/src/scope.ts
+++ acorn/src/scope.ts
@@ -4,17 +4,15 @@ import {SCOPE_VAR, SCOPE_FUNCTION, SCOPE_TOP, SCOPE_ARROW, SCOPE_SIMPLE_CATCH, B
 const pp = Parser.prototype
 
 class Scope {
-  constructor(flags) {
-    this.flags = flags
-    // A list of var-declared names in the current lexical scope
-    this.var = []
-    // A list of lexically-declared names in the current lexical scope
-    this.lexical = []
-    // A list of lexically-declared FunctionDeclaration names in the current lexical scope
-    this.functions = []
-    // A switch to disallow the identifier reference 'arguments'
-    this.inClassFieldInit = false
-  }
+  // A list of var-declared names in the current lexical scope
+  var = []
+  // A list of lexically-declared names in the current lexical scope
+  lexical = []
+  // A list of lexically-declared FunctionDeclaration names in the current lexical scope
+  functions = []
+  // A switch to disallow the identifier reference 'arguments'
+  inClassFieldInit = false
+  constructor(public flags) {}
 }
 
 // The functions in this module keep track of declared variables in the current scope in order to detect duplicate variable names.
