02032ab44859ab85b8065ef5a91cc84afb20defc
Marijn Haverbeke <marijn@haverbeke.nl>
2022-11-27 19:33:39 +0100

    Fix parsing of astral-idenfier characters after let and newline in substatement context
    FIX: Fix incorrect parse error on `if (0) let\n[astral identifier char]`.
    
    Closes https://github.com/acornjs/acorn/issues/1161

--- acorn/src/statement.js
+++ acorn/src/statement.js
@@ -42,10 +42,10 @@ pp.isLet = function(context) {
   // Statement) is allowed here. If context is not empty then only a Statement
   // is allowed. However, `let [` is an explicit negative lookahead for
   // ExpressionStatement, so special-case it first.
-  if (nextCh === 91 || nextCh === 92 || nextCh > 0xd7ff && nextCh < 0xdc00) return true // '[', '/', astral
+  if (nextCh === 91 || nextCh === 92) return true // '[', '/'
   if (context) return false
 
-  if (nextCh === 123) return true // '{'
+  if (nextCh === 123 || nextCh > 0xd7ff && nextCh < 0xdc00) return true // '{', astral
   if (isIdentifierStart(nextCh, true)) {
     let pos = next + 1
     while (isIdentifierChar(nextCh = this.input.charCodeAt(pos), true)) ++pos
