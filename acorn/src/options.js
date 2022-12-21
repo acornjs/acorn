11340a437207e7ff38f9c09268f56aecd3b8df0f
Marijn Haverbeke <marijn@haverbeke.nl>
2022-12-21 17:16:22 +0100

    Avoid crash from previous patch

--- acorn/src/options.js
+++ acorn/src/options.js
@@ -121,7 +121,7 @@ export function getOptions(opts) {
   if (options.allowReserved == null)
     options.allowReserved = options.ecmaVersion < 5
 
-  if (opts.allowHashBang == null)
+  if (!opts || opts.allowHashBang == null)
     options.allowHashBang = options.ecmaVersion >= 14
 
   if (isArray(options.onToken)) {
