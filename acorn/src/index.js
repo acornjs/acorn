5445810f6dac81f0a5f8a1b36a2f5c312173c629
Marijn Haverbeke <marijn@haverbeke.berlin>
2024-06-14 09:07:11 +0200

    Mark version 8.12.0

--- acorn/src/index.js
+++ acorn/src/index.js
@@ -31,7 +31,7 @@ import {isIdentifierChar, isIdentifierStart} from "./identifier.js"
 import {Token} from "./tokenize.js"
 import {isNewLine, lineBreak, lineBreakG, nonASCIIwhitespace} from "./whitespace.js"
 
-export const version = "8.11.3"
+export const version = "8.12.0"
 export {
   Parser,
   defaultOptions,
