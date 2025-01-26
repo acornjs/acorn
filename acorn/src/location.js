d9201ac9558d55eefc3d7b172df9b636ce27cdee
norbiros <norbiros@protonmail.com>
2025-01-26 17:29:28 +0100

    fix: Don't use string templates

--- acorn/src/location.js
+++ acorn/src/location.js
@@ -11,9 +11,9 @@ const pp = Parser.prototype
 
 pp.raise = function(pos, message) {
   let loc = getLineInfo(this.input, pos)
-  message += ` (${loc.line}:${loc.column})`
+  message += " (" + loc.line + ":" + loc.column + ")"
   if (this.sourceFile) {
-    message += ` in ${this.sourceFile}`
+    message += " in " + this.sourceFile
   }
   let err = new SyntaxError(message)
   err.pos = pos; err.loc = loc; err.raisedAt = this.pos
