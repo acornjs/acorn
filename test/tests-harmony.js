ab47d426b1b0787ee6d6a246292172964b5244a0
Huáng Jùnliàng <jlhwung@gmail.com>
2026-09-13 03:17:14 -0400

    Update test case for unexpected identifier character (#1456)
    U+2B81E will be assigned in the block CJK Unified Ideographs Extension D in Unicode 18. Thus the test case will pass when we upgrade Unicode definitions to Unicode 18.
    
    Here we change it to U+2FFFE, which guarantees to be a non-character codepoint.

--- test/tests-harmony.js
+++ test/tests-harmony.js
@@ -14833,7 +14833,7 @@ test("for (var [name, value] in obj) {}", {
 testFail("let [x]", "Complex binding patterns require an initialization value (1:7)", {ecmaVersion: 6})
 testFail("var [x]", "Complex binding patterns require an initialization value (1:7)", {ecmaVersion: 6})
 testFail("var _𖫵 = 11;", "Unexpected character '𖫵' (1:5)", {ecmaVersion: 6});
-testFail("var 𫠞_ = 12;", "Unexpected character '𫠞' (1:4)", {ecmaVersion: 6});
+testFail("var 𯿾_ = 12;", "Unexpected character '𯿾' (1:4)", {ecmaVersion: 6});
 testFail("var 𫠝_ = 10;", "Unexpected character '𫠝' (1:4)", {ecmaVersion: 5});
 testFail("if (1) let x = 10;", "Unexpected token (1:11)", {ecmaVersion: 6});
 testFail("for (;;) const x = 10;", "Unexpected token (1:9)", {ecmaVersion: 6});
